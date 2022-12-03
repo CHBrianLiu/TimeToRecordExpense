5477e051def87aeda8d472ffdb5f0d471e039366
brianliu <hong86323@gmail.com>
2022-12-03 16:36:33 +0800

    Basic text message reply

--- TimeToRecordExpense/message_handlers/__init__.py
+++ TimeToRecordExpense/message_handlers/__init__.py
@@ -1,8 +1,15 @@
+from functools import partial
+
 import linebot.models
 
 from TimeToRecordExpense import config
 from TimeToRecordExpense.message_handlers import message_event_handler
 
-handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
+from .message_event_handler import handle_text_message_event
 
-handler.add(linebot.models.MessageEvent, message=linebot.models.TextMessage)
+handler = linebot.WebhookHandler(config.LINE_CHANNEL_SECRET)
+linebot_client = linebot.LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
+
+handler.add(linebot.models.MessageEvent, message=linebot.models.TextMessage)(
+    partial(handle_text_message_event, client=linebot_client)
+)
--- TimeToRecordExpense/message_handlers/message_event_handler.py
+++ TimeToRecordExpense/message_handlers/message_event_handler.py
@@ -1,4 +1,15 @@
-from linebot.models import MessageEvent
+from linebot import LineBotApi
+from linebot.models import (MessageAction, MessageEvent, QuickReply,
+                            QuickReplyButton, TextMessage, TextSendMessage)
 
-def handle_text_message_event(event: MessageEvent):
-    pass
+done_quick_reply_button = QuickReplyButton(
+    action=MessageAction("Recorded", text="!done")
+)
+
+
+def handle_text_message_event(event: MessageEvent, client: LineBotApi):
+    message: TextMessage = event.message
+    if message.text == "!":
+        quick_reply = QuickReply(items=[done_quick_reply_button])
+        message = TextSendMessage(text="select", quick_reply=quick_reply)
+        client.reply_message(event.reply_token, message)
